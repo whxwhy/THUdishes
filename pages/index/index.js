6ac635cb05a2c25da02b5333fb9fc924d9966a87
whxwhy <1960845241@qq.com>
2025-04-01 22:10:49 +0800

    5.1.1
    其实版本号完全不对啊1！！加了首页地图

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,23 +5,79 @@ Page({
    * 页面的初始数据
    */
   data: {
-    canteenData:[],
-    longitude:116.326836,latitude:40.00135,scale:15,
-    markers:[{id:0,longitude:116.326836,latitude:40.00366,width:30,height:30,callout:{content:"wwwww",display:'ALWAYS'}}]
+    mapId:"myMap",
+    longitude:116.326836,latitude:40.00485,scale:15,
+    markers:[
+      {
+        id:0,
+        longitude:116.32871,
+        latitude:40.01161,
+        width:30,
+        height:30,
+        callout:{
+          content:"紫荆园",
+          display:'ALWAYS',
+          padding:6
+      }},
+     {
+       id:1,
+        longitude:116.32634,
+        latitude:40.01083,
+       width:30,
+       height:30,
+       callout:{
+        content:"桃李园",
+       display:'ALWAYS',
+       padding:6
+      }}, 
+      {
+        id:2,
+       longitude:116.32702,
+       latitude:40.00631,
+        width:30,
+        height:30,
+        callout:{
+         content:"听涛园",
+          display:'ALWAYS',
+          padding:6
+     }},
+      {
+        id:3,
+        longitude:116.328152,
+        latitude:40.00586,
+        width:30,
+        height:30,
+        callout:{
+          content:"清芬园",
+          display:'ALWAYS',
+          padding:6
+        }},
+        {
+          id:4,
+          longitude:116.32257,
+          latitude:40.00684,
+          width:30,
+          height:30,
+          callout:{
+            content:"观畴园",
+            display:'ALWAYS',
+            padding:6
+        }},
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(){
-    wx.cloud.database().collection("canteenData")
+    /**wx.cloud.database().collection("canteenData")
     .get()
     .then((res) => {
       console.log('success',res);
      this.setData({
        canteenData:res.data
      })
-    });/**wx.getLocation({
+    })*//**wx.getLocation({
       type: 'gcj02', //返回可以用于wx.openLocation的经纬度
       success (res) {
         const latitude = res.latitude
@@ -83,6 +139,32 @@ Page({
   onShareAppMessage() {
 
   },
+  toDetail:async function(e){
+    console.log('success',e);
+    const element = this.data.markers.find(item => item.id === e.markerId);
+    if (element) {
+      console.log('找到的元素:', element);
+      this.setData({
+        foundElement: element
+      });
+    } else {
+      console.log('未找到对应的元素');
+    }
+    wx.cloud.database().collection("canteenData")
+    .where({name:element.callout.content})
+    .get()
+    .then((res) => {
+      console.log('success',res);
+     wx.navigateTo({url:'/pages/canteenDetail/canteenDetail?id='+res.data[0]._id,})
+    });
+    console.log('monitor',this.data);
   
+    },
+    moveTolocation:function(){
+      let Id=this.data.mapId
+     wx.createMapContext(Id).moveToLocation({
+      longitude:116.326836,latitude:40.00485
+     });
+    }
 })
 

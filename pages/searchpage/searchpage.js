dd9c25fbbf60a03aa5be198b960d9b65e8de7e77
whxwhy <1960845241@qq.com>
2025-04-05 14:19:03 +0800

    5.1.5
    完成搜索框

--- pages/searchpage/searchpage.js
+++ pages/searchpage/searchpage.js
@@ -5,16 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    searchText: '',
+    dishData: [] 
   },
   handleInput(e){
     console.log(e);
-    const{value}=e.detail;
-    if(!value.trim()){
-      return;
-    }
-    else{}
+    this.setData({
+      searchText: e.detail.value
+    });
+    this.searchData();
   },
+ async searchData(){ 
+   const { searchText } = this.data;
+   console.log( searchText);
+ if (!searchText) {
+   // 如果搜索框为空，则不进行搜索
+   return;}
+  try {
+    const db = wx.cloud.database();
+    const res = await db.collection('dishData').where(
+      db.command.or([
+        { name: db.RegExp({ regexp: searchText, options: 'i' }) },
+        { canteen: db.RegExp({ regexp: searchText, options: 'i' }) },
+        { window: db.RegExp({ regexp: searchText, options: 'i' }) },
+        // ... 其他需要搜索的字段
+      ])
+    ).get();
+    console.log(res);
+    this.setData({
+      dishData: res.data
+    });
+  } catch (err) {
+    console.error('搜索数据失败', err);
+    this.setData({
+      dataList: []
+    });
+  }
+},
   /**wx.cloud.database().collection("dishData")
   .where({
     window: windowA.name
